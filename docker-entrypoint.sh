a062f04a187799453ce7362ef3e092eeae680b19
Marc Kohaupt <debuglevel@gmail.com>
2020-09-10 09:53:32 +0000

    Handle errors in docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,18 @@ set -e
 
 echo "Updating MikTex package repository..."
 mpm --update-db
-echo "Updating MikTex package repository done"
+RC=$?
+echo "Updated MikTex package repository (return code: $RC)"
+if [ $retVal -ne 0 ]; then
+    echo "mpm returned with an error."
+    echo "Printing /miktex/.miktex/texmfs/data/miktex/log/mpmcli.log which should contain some logs:"
+    echo "==========="
+    cat /miktex/.miktex/texmfs/data/miktex/log/mpmcli.log
+    echo "==========="
+    echo "Printed /miktex/.miktex/texmfs/data/miktex/log/mpmcli.log"
+    echo "Exiting docker-entrypoint.sh with return code $RC"
+    exit $RC
+fi
 
 # if environment variable INSTALL_PACKAGES is set, install those packages already at startup
 if [[ ! -z "$INSTALL_PACKAGES" ]]; then
@@ -12,7 +23,8 @@ if [[ ! -z "$INSTALL_PACKAGES" ]]; then
     do
       echo "Installing package $PACKAGE..."
       mpm --require $PACKAGE
-      echo "Installing package $PACKAGE done."
+      RC=$?
+      echo "Installed package $PACKAGE done (return code: $RC)"
     done
 fi
 
